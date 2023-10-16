a20015054578af8acbade07746e0fc54b7666eda
alxgunin <gunin2004@yandex.ru>
2023-10-16 14:45:06 +0300

    task

--- build/tests/CMakeFiles/tests_coverage.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tests_coverage"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tests_coverage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
